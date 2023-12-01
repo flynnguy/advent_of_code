4158dba1c43ab80c53ee13ba44147915a9fc6e47
Christopher Flynn <chris.flynn@penn-interactive.com>
2023-12-01 17:05:01 -0500

    Add tests to python code as well

--- 2023/python/day1/day1.py
+++ 2023/python/day1/day1.py
@@ -15,9 +15,19 @@ numbers = {
     "nine": "9",
 }
 
-def day1(part):
+def day1(lines, part):
+    """
+    Test examples
+
+    >>> lines = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
+    >>> day1(lines, 1)
+    142
+
+    >>> lines = ["two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen"]
+    >>> day1(lines, 2)
+    281
+    """
     total = 0
-    lines = get_data(day=1, year=2023).split("\n")
 
     for line in lines:
         values = []
@@ -34,5 +44,7 @@ def day1(part):
     return total
 
 if __name__ == "__main__":
-    print(f"Part 1 solution: {day1(1)}") # Should be 54630
-    print(f"Part 2 solution: {day1(2)}") # Should be 54770
+    lines = get_data(day=1, year=2023).split("\n")
+
+    print(f"Part 1 solution: {day1(lines, 1)}") # Should be 54630
+    print(f"Part 2 solution: {day1(lines, 2)}") # Should be 54770
